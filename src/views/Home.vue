15868881e24ba7a582e787088989619918b16433
MichaelLuo520 <po2319987@gmail.com>
2021-12-13 02:52:50 +0800

    update homePage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,121 @@
 <template>
   <div class="home">
-    <div class="home_banner-section"></div>
-
-    <h1></h1>
+    <div class="home_banner-section">
+      <div class="home_banner">
+        <div class="home_banner-image"></div>
+        <div class="home_banner-content">
+          <h1 class="home_banner-title">
+            Commercial builders with pride –
+            workmanship that values quality.
+          </h1>
+          <p class="body1 home_banner-subTitle">
+            Acclaim Contractors are a highly specialised local business with over 20 years building and civil industry
+            experience with many accolades, commendations and awards recognising their workmanship.Acclaim
+            Contractors are a highly specialised local business with over 20 years building .
+          </p>
+        </div>
+        <header>
+          <div class="nav_logo">
+            <svg
+              width="23"
+              height="5"
+              viewBox="0 0 23 5"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M2 2.54999H21" stroke="white" stroke-width="4" stroke-linecap="square" />
+            </svg>
+            <svg
+              width="27"
+              height="36"
+              viewBox="0 0 27 36"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M11.0159 35.3717C9.25588 35.3717 7.4799 35.3145 5.68789 35.2003C3.89588 35.0861 2.32789 34.8494 0.983887 34.4904V1.5403C1.68789 1.40974 2.43988 1.28734 3.23989 1.1731C4.03989 1.05886 4.85588 0.969098 5.68789 0.903818C6.51989 0.838537 7.34388 0.789578 8.15989 0.756938C8.97589 0.724297 9.75188 0.707977 10.4879 0.707977C12.5039 0.707977 14.3759 0.863016 16.1039 1.1731C17.8319 1.48318 19.3279 1.99725 20.5919 2.71534C21.8559 3.43342 22.8399 4.37997 23.5439 5.55502C24.2479 6.73006 24.5999 8.18253 24.5999 9.91246C24.5999 11.5118 24.2239 12.8827 23.4719 14.0251C22.7199 15.1675 21.6719 16.0814 20.3279 16.7669C22.3439 17.4523 23.8319 18.4641 24.7919 19.8024C25.7519 21.1406 26.2319 22.8379 26.2319 24.8942C26.2319 28.3867 24.9839 31.0061 22.4879 32.7523C19.9919 34.4985 16.1679 35.3717 11.0159 35.3717ZM6.93589 19.7045V30.035C7.60789 30.1003 8.32788 30.1493 9.09589 30.1819C9.86389 30.2145 10.5679 30.2309 11.2079 30.2309C12.4559 30.2309 13.6159 30.1493 14.6879 29.9861C15.7599 29.8229 16.6879 29.5373 17.4719 29.1293C18.2559 28.7213 18.8799 28.1745 19.3439 27.4891C19.8079 26.8037 20.0399 25.9224 20.0399 24.8453C20.0399 22.9195 19.3519 21.5813 17.9759 20.8305C16.5999 20.0798 14.6959 19.7045 12.2639 19.7045H6.93589ZM6.93589 14.8574H11.2079C13.5119 14.8574 15.3199 14.5229 16.6319 13.8537C17.9439 13.1846 18.5999 11.9851 18.5999 10.2552C18.5999 8.62317 17.9199 7.46446 16.5599 6.77902C15.1999 6.09357 13.4319 5.75086 11.2559 5.75086C10.3279 5.75086 9.50389 5.76718 8.78389 5.79982C8.06388 5.83246 7.44789 5.88142 6.93589 5.9467V14.8574Z"
+                fill="white"
+              />
+            </svg>
+          </div>
+          <div class="nav-bar"></div>
+          <div class="nav-hamburger">
+            <svg
+              width="20"
+              height="2"
+              viewBox="0 0 20 2"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M1.01671 0.630423H18.9834" stroke="white" stroke-linecap="round" />
+            </svg>
+            <svg
+              width="20"
+              height="2"
+              viewBox="0 0 20 2"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M1.01671 0.630423H18.9834" stroke="white" stroke-linecap="round" />
+            </svg>
+            <svg
+              width="20"
+              height="2"
+              viewBox="0 0 20 2"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M1.01671 0.630423H18.9834" stroke="white" stroke-linecap="round" />
+            </svg>
+          </div>
+        </header>
+        <p class="body2 home_banner-siderTitle">Block Studio©2018 Copyright. All Rights Reserved.</p>
+        <div class="home_media">
+          <svg
+            width="10"
+            height="18"
+            viewBox="0 0 10 18"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            class="facebook"
+          >
+            <path
+              d="M6.39703 17.997V9.80101H9.16203L9.57303 6.59202H6.39703V4.54802C6.39703 3.62202 6.65503 2.98802 7.98403 2.98802H9.66803V0.127017C8.84867 0.0392076 8.02508 -0.0031906 7.20103 1.65784e-05C4.75703 1.65784e-05 3.07903 1.49202 3.07903 4.23102V6.58602H0.332031V9.79502H3.08503V17.997H6.39703Z"
+              fill="black"
+            />
+          </svg>
+          <svg
+            width="20"
+            height="20"
+            viewBox="0 0 20 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            class="instagram"
+          >
+            <path
+              d="M10.0001 5.19295C7.33995 5.19295 5.19308 7.33983 5.19308 9.99998C5.19308 12.6601 7.33995 14.807 10.0001 14.807C12.6603 14.807 14.8071 12.6601 14.8071 9.99998C14.8071 7.33983 12.6603 5.19295 10.0001 5.19295ZM10.0001 13.1242C8.2798 13.1242 6.87589 11.7203 6.87589 9.99998C6.87589 8.27967 8.2798 6.87576 10.0001 6.87576C11.7204 6.87576 13.1243 8.27967 13.1243 9.99998C13.1243 11.7203 11.7204 13.1242 10.0001 13.1242ZM15.004 3.87576C14.3829 3.87576 13.8814 4.37733 13.8814 4.99842C13.8814 5.61951 14.3829 6.12108 15.004 6.12108C15.6251 6.12108 16.1267 5.62186 16.1267 4.99842C16.1269 4.85094 16.0979 4.70487 16.0416 4.56858C15.9852 4.43229 15.9026 4.30845 15.7983 4.20417C15.694 4.09988 15.5701 4.0172 15.4339 3.96084C15.2976 3.90449 15.1515 3.87558 15.004 3.87576ZM19.3704 9.99998C19.3704 8.70623 19.3821 7.4242 19.3095 6.1328C19.2368 4.6328 18.8946 3.30155 17.7978 2.20467C16.6985 1.10545 15.3696 0.765608 13.8696 0.692952C12.5759 0.620295 11.2939 0.632014 10.0025 0.632014C8.7087 0.632014 7.42667 0.620295 6.13527 0.692952C4.63527 0.765608 3.30402 1.1078 2.20714 2.20467C1.10792 3.30389 0.76808 4.6328 0.695424 6.1328C0.622767 7.42655 0.634486 8.70858 0.634486 9.99998C0.634486 11.2914 0.622767 12.5758 0.695424 13.8672C0.76808 15.3672 1.11027 16.6984 2.20714 17.7953C3.30636 18.8945 4.63527 19.2344 6.13527 19.307C7.42902 19.3797 8.71105 19.368 10.0025 19.368C11.2962 19.368 12.5782 19.3797 13.8696 19.307C15.3696 19.2344 16.7009 18.8922 17.7978 17.7953C18.897 16.6961 19.2368 15.3672 19.3095 13.8672C19.3845 12.5758 19.3704 11.2937 19.3704 9.99998ZM17.3079 15.5265C17.1368 15.9531 16.9306 16.2719 16.6001 16.6C16.2696 16.9305 15.9532 17.1367 15.5267 17.3078C14.2939 17.7976 11.3665 17.6875 10.0001 17.6875C8.6337 17.6875 5.70402 17.7976 4.4712 17.3101C4.04464 17.139 3.72589 16.9328 3.39777 16.6023C3.0673 16.2719 2.86105 15.9555 2.68995 15.5289C2.20246 14.2937 2.31261 11.3664 2.31261 9.99998C2.31261 8.63358 2.20246 5.70389 2.68995 4.47108C2.86105 4.04451 3.0673 3.72576 3.39777 3.39764C3.72824 3.06951 4.04464 2.86092 4.4712 2.68983C5.70402 2.20233 8.6337 2.31248 10.0001 2.31248C11.3665 2.31248 14.2962 2.20233 15.529 2.68983C15.9556 2.86092 16.2743 3.06717 16.6025 3.39764C16.9329 3.72811 17.1392 4.04451 17.3103 4.47108C17.7978 5.70389 17.6876 8.63358 17.6876 9.99998C17.6876 11.3664 17.7978 14.2937 17.3079 15.5265Z"
+              fill="black"
+            />
+          </svg>
+          <svg
+            width="20"
+            height="16"
+            viewBox="0 0 20 16"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            class="youtube"
+          >
+            <path
+              d="M19.543 2.498C20 4.28 20 8 20 8C20 8 20 11.72 19.543 13.502C19.289 14.487 18.546 15.262 17.605 15.524C15.896 16 10 16 10 16C10 16 4.107 16 2.395 15.524C1.45 15.258 0.708 14.484 0.457 13.502C2.98023e-08 11.72 0 8 0 8C0 8 2.98023e-08 4.28 0.457 2.498C0.711 1.513 1.454 0.738 2.395 0.476C4.107 -1.78814e-07 10 0 10 0C10 0 15.896 -1.78814e-07 17.605 0.476C18.55 0.742 19.292 1.516 19.543 2.498ZM8 11.5L14 8L8 4.5V11.5Z"
+              fill="black"
+            />
+          </svg>
+        </div>
+      </div>
+    </div>
+    <h1>1312313</h1>
   </div>
 
   <div class="btn btn-primary">123</div>
@@ -32,16 +145,96 @@ export default {
 </script>
 
 <style lang="scss">
-.home {
+.home_banner-section {
   max-width: 1280px;
-  .home_banner-section {
-    height: 100vh;
+  &::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
     width: 100vw;
-    // padding-top: 114px;
-    background-size: cover;
-    background-position: 50%;
-    background-attachment: fixed;
-    background-image: url("https://picsum.photos/2000/2000");
+    height: 100vh;
+    background-color: #000;
+    opacity: 0.3;
+  }
+  .home_banner {
+    .home_banner-image {
+      height: 100vh;
+      width: 100vw;
+      background-size: cover;
+      background-position: 50%;
+      background-attachment: fixed;
+      background-image: url("https://picsum.photos/2000/2000?grayscale");
+    }
+    .home_banner-content {
+      position: absolute;
+      top: 40%;
+      // margin: 304px 201px 348px 202px;
+      margin: 0px 201px 0px 202px;
+
+      .home_banner-title {
+        color: #fff;
+        padding-bottom: 28px;
+        margin: 0px 201px 0px 202px;
+      }
+      .home_banner-subTitle {
+        color: #fff;
+        margin: 0px 342px 0px 343px;
+        text-align: center;
+      }
+    }
+    .home_banner-siderTitle {
+      color: #fff;
+      transform: rotate(90deg);
+      position: absolute;
+      left: -40px;
+      top: 50%;
+    }
+    .home_media {
+      position: absolute;
+      left: 0;
+      bottom: 0;
+      padding: 0 0 26px 44px;
+      svg {
+        margin-right: 66.93px;
+      }
+    }
+  }
+}
+
+header {
+  .nav_logo {
+    display: flex;
+    flex-direction: column;
+    position: absolute;
+    top: 0;
+    left: 0;
+    padding: 26.55px 0 0 44.98px;
+    svg {
+      &:first-child {
+        margin-bottom: 13.16px;
+      }
+    }
+  }
+  .nav-hamburger {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    /* Rectangle 2 */
+    position: absolute;
+    width: 95px;
+    height: 100px;
+    top: 0px;
+    right: 0px;
+    /* Black */
+    background: #000000;
+    svg {
+      margin-top: 7.97px;
+      &:first-of-type {
+        margin-top: 0;
+      }
+    }
   }
 }
 </style>
