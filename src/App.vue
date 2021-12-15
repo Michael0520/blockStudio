7325f924d3c141652a8a4f11cd5bfbe72f1eedc2
MichaelLuo520 <po2319987@gmail.com>
2021-12-15 21:09:47 +0800

    reset global a-link styles

--- src/App.vue
+++ src/App.vue
@@ -11,23 +11,16 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   width: 100%;
 }
-
-#nav {
-  // padding: 30px;
-
-  a {
+a {
+  color: inherit; /* blue colors for links too */
+  text-decoration: inherit; /* no underline */
+  &:hover {
+    color: inherit; /* blue colors for links too */
     font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
   }
 }
-
 body {
   margin: 0;
   overflow-x: hidden;
